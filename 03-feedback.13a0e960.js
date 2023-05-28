2c6c5cbe9d68d875eb9045ce5fd985215c4f4e01
Poluvko-Tatyana <117657365+Poluvko-Tatyana@users.noreply.github.com>
2023-05-28 08:10:02 +0000

    Deploying to gh-pages from @ Poluvko-Tatyana/goit-js-hw-08@faa0d8cab83a2c1fad0402cd4d9e1fa87b1c31f7 🚀

---
+++ 03-feedback.13a0e960.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");let t={};console.log(t),e.addEventListener("input",(function(e){localStorage.setItem("feedback-form-state",JSON.stringify(t))}));
+//# sourceMappingURL=03-feedback.13a0e960.js.map
---
+++ 03-feedback.3e269b4d.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form"),t={};console.log(t),e.addEventListener("input",(function(e){localStorage.setItem("feedback-form-state",JSON.stringify(t))}))}();
+//# sourceMappingURL=03-feedback.3e269b4d.js.map
