faa0d8cab83a2c1fad0402cd4d9e1fa87b1c31f7
Poluvko-Tatyana <poluvko.tatyana@gmail.com>
2023-05-28 11:09:38 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItem = galleryItems.map (({preview, original, description}) => {
+    const galleryList =`
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class='gallery__image'
+    src="${preview}"
+    alt="${description}"
+     />
+    </a>
+    </li>`;
+    return galleryList;
+})
+
+gallery.insertAdjacentHTML('beforeend', galleryItem.join(''));
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250, 
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+const form = document.querySelector('.feedback-form');
+const KeyFeedbackFormState = "feedback-form-state";
+let value = {};
+console.log(value)
+form.addEventListener("input", onClickInput);
 
+function onClickInput(evt){
+    localStorage.setItem(KeyFeedbackFormState, JSON.stringify(value))
+    
+   
+}
