2823507ff98a2fdf2c9bc24ba9b994d41fe0a801
Poluvko-Tatyana <poluvko.tatyana@gmail.com>
2023-05-28 21:14:13 +0300

    2 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from "lodash.throttle";
+import Vimeo from '@vimeo/player'
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const KayStorage = "videoplayer-current-time";
+
+player.on('timeupdate', throttle (function (data){
+    localStorage.setItem(KayStorage, JSON.stringify(data.seconds));
+}),1000);
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(KayStorage))).then(function(seconds){
+    
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
